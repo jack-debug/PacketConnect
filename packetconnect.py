ffb3fbd277d908a3902e6721a8389530e45b4f40
jack-debug <55571956+jack-debug@users.noreply.github.com>
2021-03-22 14:43:36 +1100

    Update packetconnect.py

--- packetconnect.py
+++ packetconnect.py
@@ -10,33 +10,6 @@ def clearConsole():
     if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
         command = 'cls'
     os.system(command)
-def Encryption(s,k):
-    encstr=""
-    for i in s:
-        if(ord(i))>=65 and (ord(i)<=90):
-            temp=(ord(i)+k)
-            if temp>90:
-                temp=temp%90+64
-            encstr=encstr+chr(temp)
-        elif(ord(i))>=97 and (ord(i)<=122):
-            temp=(ord(i)+k)
-            if temp>122:
-                temp=temp%122+96
-            encstr=encstr+chr(temp)
-        else:
-            encstr=encstr+chr(ord(i)+k)
-    return encstr
-def Decryption(k):
-    p=Encryption(s,k)
-    decstr=""
-    for i in p:
-        if((ord(i))>=65) and (ord(i))<=90:
-            decstr=decstr+chr((ord(i) - k-65) % 26 + 65)
-        elif((ord(i))>=97) and (ord(i))<=122:
-            decstr=decstr+chr((ord(i) - k - 97) % 26 + 97)
-        else:
-            decstr=decstr+chr(ord(i)-k)
-    return decstr
 
 k = random.randint(1,2000)
 cipher_suite = Fernet(k)
